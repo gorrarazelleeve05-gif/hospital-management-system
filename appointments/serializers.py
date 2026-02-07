b63e449b85458c4d81009e0e80ceb54565bfd501
gorrarazelleeve05-gif <gorra.razelleeve05@gmail.com>
2026-02-07 18:14:24 +0800

    Added hyperlinked URLs to serializers for easier navigation

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -2,19 +2,19 @@ from rest_framework import serializers
 from .models import Patient, Doctor, Appointment
 
 
-class PatientSerializer(serializers.ModelSerializer):
+class PatientSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Patient
-        fields = '__all__'
+        fields = ['url', 'patient_id', 'patient_name', 'address', 'contact_no', 'age', 'sex']
 
 
-class DoctorSerializer(serializers.ModelSerializer):
+class DoctorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Doctor
-        fields = '__all__'
+        fields = ['url', 'doctor_id', 'doctor_name', 'specialization', 'sex', 'contact_no']
 
 
-class AppointmentSerializer(serializers.ModelSerializer):
+class AppointmentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Appointment
-        fields = ['appt_id', 'doctor', 'patient', 'appt_date', 'appt_time', 'room_no', 'bldg_no']
+        fields = ['url', 'appt_id', 'doctor', 'patient', 'appt_date', 'appt_time', 'room_no', 'bldg_no']
